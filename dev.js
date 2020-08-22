4a3f576bf7958ca187ecf32008dee9a4f3f92ddc
Kumar Gaurav <61309195+ningenda@users.noreply.github.com>
2020-08-22 17:28:03 +0530

    Add files via upload

---
+++ dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: '88780693579-34i4a3jfq0q15eg7kn10dbqf6rpu9if8.apps.googleusercontent.com',
+  googleClientSecret: 'kl8zUPpOSWvan4vJjOoaiJL5',
+  mongoURI : 'mongodb+srv://ningen:mongodb2020@emaily.xtsm0.mongodb.net/emaily-dev?retryWrites=true&w=majority',
+  cookieKey: 'hshdgfkwjfgjkasfbbkjbsabjksdfbdhbsfsjhdbjhsgjdnjksd'
+  //PBqQ73Zrx0IrxxOa//
+
+};
