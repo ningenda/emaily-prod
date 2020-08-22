e7eb6f3ac60080accf6e62f8f690c1192b94b7e2
Kumar Gaurav <61309195+ningenda@users.noreply.github.com>
2020-08-22 17:31:13 +0530

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID: '88780693579-34i4a3jfq0q15eg7kn10dbqf6rpu9if8.apps.googleusercontent.com',
-  googleClientSecret: 'kl8zUPpOSWvan4vJjOoaiJL5',
-  mongoURI : 'mongodb+srv://*****:******@emaily.xtsm0.mongodb.net/******?retryWrites=true&w=majority',
-  cookieKey: 'hshdgfkwjfgjkasfbbkjbsabjksdfbdhbsfsjhdbjhsgjdnjksd'
+  googleClientID: '**********************************************************',
+  googleClientSecret: '********************',
+  mongoURI : 'mongodb+srv://*****:******@*********.xtsm0.mongodb.net/******?retryWrites=true&w=majority',
+  cookieKey: '*******************************************'
   //PBqQ73Zrx0IrxxOa//
 
 };
